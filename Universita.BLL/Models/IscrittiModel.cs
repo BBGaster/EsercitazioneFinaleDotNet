2a4898c279e7f678c1a1dff838031ed434187f21
Bruno Belviso <belbruno2004@gmail.com>
2024-07-09 16:23:39 +0200

    Ultima

--- Universita.BLL/Models/IscrittiModel.cs
+++ Universita.BLL/Models/IscrittiModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Universita.BLL.Models
@@ -10,10 +11,12 @@ namespace Universita.BLL.Models
     public class IscrittiModel
     {
         
-        public int StudenteMatricola { get; set; } 
+        public int StudenteMatricola { get; set; }
+        
         public StudenteModel? Studente { get; set; }
 
         public int CorsoId { get; set; }
+        
         public CorsoModel? Corso { get; set; }
     }
 }
